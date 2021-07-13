6d7a1e86e1ce70f9b786880887cb483a7ad0b122
Rupali Lahoti <rupalee28@gmail.com>
2021-07-13 08:46:24 +0530

    Added in A1

--- src/package1/class1.java
+++ src/package1/class1.java
@@ -4,7 +4,8 @@ public class class1 {
 	
 	public static void main(String[] args) {
 		System.out.println("In class A");
-		System.out.println("1.1 in branch A1");
+		System.out.println("1.1 in branch B1");
+		System.out.println("B1");
 	}
 	
 
